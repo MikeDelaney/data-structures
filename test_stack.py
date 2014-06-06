9bb2ec0a12db1f9055cbdccf85e1c852eea35527
Jonathan Siebert <jon.siebert@gmail.com>
2014-06-06 16:56:30 -0700

    Separated tests

--- test_stack.py
+++ test_stack.py
@@ -1,42 +1,53 @@
 import pytest
 from stack import Stack
+from stack import node
+
+
+stack = None
+node1 = None
+node2 = None
+@pytest.fixture(scope="function")
+def init_test_stack():
+    node1 = node(1, None)
+    node2 = node(2, node1)
+    stack = Stack()
+    stack.head = node2
 
 
 def test_new_stack():
     stack = Stack()
     assert stack.head is None
 
-def test_stack_push():
+
+def test_stack_push_empty():
     stack = Stack()
     stack.push(1)
     assert stack.head.value == 1
     assert stack.head.next_node is None
 
+
+def test_stack_push(init_test_stack):
     previous_head = stack.head
-    stack.push(2)
-    assert stack.head.value == 2
-    assert stack.head.next_node is previous_head
-    assert previous_head.value == 1
-    assert previous_head.next_node is None
-
-
-def test_stack_pop():
-    stack = Stack()
-    stack.push(1)
-    index_1 = stack.head
-    stack.push(2)
-    index_2 = stack.head
     stack.push(3)
+    assert stack.head.value == 3
+    assert stack.head.next_node is previous_head
+    assert previous_head.value == 2
 
-    assert stack.pop() == 3
-    assert stack.head is index_2
 
+def test_stack_pop(init_test_stack):
     assert stack.pop() == 2
-    assert stack.head is index_1
+    assert stack.head is node1
 
+
+def test_stack_pop_head(init_test_stack):
+    stack.pop()
     stack.pop()
     assert stack.head is None
 
+
+def test_stack_pop_empty(init_test_stack):
+    stack.pop()
+    stack.pop()
     try:
         stack.pop()
     except LookupError:
@@ -45,5 +56,5 @@ def test_stack_pop():
         raise AssertionError("Exception other than LookupError raised")
     else:
         raise AssertionError(
-            "No exception raised when when popping from an empty stack"
+            "No exception raised when popping from an empty stack"
         )
