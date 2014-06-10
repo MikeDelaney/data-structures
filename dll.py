3b81047e0dca078735ed991a0cb631ffe18f3fce
Jonathan Siebert <jon.siebert@gmail.com>
2014-06-10 14:33:22 -0700

    Most of Dll class implemented

--- dll.py
+++ dll.py
@@ -7,16 +7,31 @@ class Dll(object):
     def __init__(self, head=None, tail=None):
         self.tail = self.head = None
 
-    def insert(self, val):
-        pass
+    def insert(self, value):
+        if self.head is None:
+            self.head = self.tail = Node(val=value)
+        else:
+            new_node = Node(val=value, next_node=self.head)
+            self.head.prev = new_node
+            self.head = new_node
 
-    def append(self, val):
-        pass
+    def append(self, value):
+        if self.tail is None:
+            self.tail = self.head = Node(val=value)
+        else:
+            new_node = Node(val=value, prev=self.tail)
+            self.tail.next_node = new_node
+            self.tail = new_node
 
-    def pop():
-        pass
+    def pop(self):
+        if self.head is None:
+            raise LookupError
+        retval = self.head.value
+        self.head = self.head.next_node
+        self.head.prev = None
+        return retval
 
-    def shift():
+    def shift(self):
         pass
 
     def remove(self, val):
