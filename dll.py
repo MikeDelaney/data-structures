ea043c6b16b0e8ac6c5c419a697623866b4ee9b2
MikeDelaney <mdelaney@utexas.edu>
2014-06-10 13:50:38 -0700

    Add Node and Dll classes

---
+++ dll.py
@@ -0,0 +1,23 @@
+class Node(object):
+    def __init__(self, prev=None, val=None, next_node=None):
+        self.prev, self.value, self.next_node = prev, val, next_node
+
+
+class Dll(object):
+    def __init__(self, head=None, tail=None):
+        self.tail = self.head = None
+
+    def insert(self, val):
+        pass
+
+    def append(self, val):
+        pass
+
+    def pop():
+        pass
+
+    def shift():
+        pass
+
+    def remove(self, val):
+        pass
