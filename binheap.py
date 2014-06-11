44a92a36a728f8388ce295c7b46a0c1079ffff61
Jonathan Siebert <jon.siebert@gmail.com>
2014-06-11 13:37:21 -0700

    Tests for binheap.__init__

--- binheap.py
+++ binheap.py
@@ -1,3 +1,18 @@
 # -*- charset: utf-8 -*-
 
 
+class binheap(list):
+    def comp_min(self, x, y):
+        pass
+
+    def comp_max(self, x, y):
+        pass
+
+    def __init(self, init, minmax="min"):
+        if minmax == "min":
+            self.compare = binheap.comp_min
+        else:
+            self.compare = binheap.comp_max
+
+    def heapify(self):
+        pass
--- test_binheap.py
+++ test_binheap.py
@@ -1,16 +1,42 @@
 # -*- charset: utf-8 -*-
 
+import pytest
 from binheap import binheap
 
 
+@pytest.fixture(scope="function")
+def init_list():
+    raw = [1, -3, 5, 7, -9]
+    min_heap = [-9, -3, 5, 7, 1]
+    max_heap = [7, 5, 1, -3, -9]
+    return raw, min_heap, max_heap
+
+
 def test_comp_min():
-    assert binheap.compmin(5, 4) is False
-    assert binheap.compmin(-5, -4) is True
+    heap = binheap()
+    assert heap.comp_min(5, 4) is False
+    assert heap.comp_min(-5, -4) is True
 
 
 def test_comp_max():
-    assert binheap.compmax(5, 4) is True
-    assert binheap.compmax(-5, -4) is False
+    heap = binheap()
+    assert heap.comp_max(5, 4) is True
+    assert heap.comp_max(-5, -4) is False
 
 
+def test_binheap_init_default():
+    heap = binheap()
+    assert len(heap) == 0
+    assert heap.compare is binheap.comp_max
 
+
+def test_binheap_init_iter_min(init_list):
+    raw, min_heap, max_heap = init_list
+    heap = binheap(raw, minmax="min")
+    assert heap == min_heap
+
+
+def test_binheap_init_iter_max(init_list):
+    raw, min_heap, max_heap = init_list
+    heap = binheap(raw, minmax="max")
+    assert heap == max_heap
