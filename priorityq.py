8a36b4df41504b71445bd93dd4724992ff319979
Jonathan Siebert <jon.siebert@gmail.com>
2014-06-12 13:52:36 -0700

    Created Item, Priorityq stub

---
+++ priorityq.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from binheap import Binheap
+
+
+class Item(object):
+    def __init__(self, priority, value):
+        self.priority = priority
+        self.value = value
+
+    def __cmp__(self, other):
+        return self.priority < other.priority
+
+class Priorityq(Binheap):
+    pass
