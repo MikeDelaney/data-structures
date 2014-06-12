bef78c3dd421e84ec58160c5f3d246dd915962c5
MikeDelaney <mdelaney@utexas.edu>
2014-06-11 23:13:43 -0700

    Implement push and pop functions, change names for pep8, fix tests

--- binheap.py
+++ binheap.py
@@ -1,40 +1,75 @@
 # -*- charset: utf-8 -*-
 
 
-class binheap(list):
-    def __init__(self, init=None, minmax="min"):
+class Binheap(object):
+    """
+    Represents a binary heap as a list created from iterable. If no iterable,
+    creates empty heap. minmax allows creation of max-heap and
+    minheap objects.
+    """
+    def __init__(self, iterable=None, minmax="min"):
+        if iterable is not None:
+            self._list = list(iterable)
+        else:
+            self._list = []
         if minmax == "min":
+            self.minmax = "min"
             self.compare = self.comp_min
         else:
+            self.minmax = "max"
             self.compare = self.comp_max
-        if init is not None:
-            self.extend(init)
-            self._heapify()
+        self._build()
 
     def comp_min(self, x, y):
+        """
+        Comparison for min-heap
+        """
         return x < y
 
     def comp_max(self, x, y):
+        """
+        Comparison for max-heap
+        """
         return x > y
 
     def _build(self):
-        for index in range(len(self)//2, -1, -1):
+        """
+        Traverses levels of heap. Sends parent 'nodes' to _heapify.
+        """
+        for index in range(len(self._list)//2, -1, -1):
             self._heapify(index)
 
     def _heapify(self, index):
+        """
+        Heapifies a single subtree.
+        """
         left = 2 * index + 1
         right = 2 * index + 2
         target = index
-        if left <= len(self)-1 and self.compare(self[left], self[target]):
-            target = left
-        if right <= len(self)-1 and self.compare(self[right], self[target]):
+        if left <= len(self._list)-1:
+            if self.compare(self._list[left], self._list[target]):
+                target = left
+        if right <= len(self._list)-1:
+            if self.compare(self._list[right], self._list[target]):
                 target = right
         if target != index:
-            self[index], self[target] = self[target], self[index]
+            self._list[index], self._list[target] = (
+                self._list[target], self._list[index])
             self._heapify(target)
 
     def push(self, value):
-        pass
+        """
+        Adds value to end of heap and heapifies the tree.
+        """
+        self._list.append(value)
+        self._build()
 
     def pop(self):
-        pass
+        """
+        Removes root of heap, replaces with last value, rebuilds tree.
+        """
+        last = self._list.pop()
+        copy = self._list[1:]
+        copy.insert(0, last)
+        temp = Binheap(copy, self.minmax)
+        self._list = temp._list
--- test_binheap.py
+++ test_binheap.py
@@ -1,7 +1,7 @@
 # -*- charset: utf-8 -*-
 
 import pytest
-from binheap import binheap
+from binheap import Binheap
 
 
 @pytest.fixture(scope="function")
@@ -13,82 +13,101 @@ def init_list():
 
 
 def test_comp_min():
-    heap = binheap()
+    heap = Binheap()
     assert heap.comp_min(5, 4) is False
     assert heap.comp_min(-5, -4) is True
 
 
 def test_comp_max():
-    heap = binheap()
+    heap = Binheap()
     assert heap.comp_max(5, 4) is True
     assert heap.comp_max(-5, -4) is False
 
 
 def test_binheap_init_default():
-    heap = binheap()
-    assert len(heap) == 0
+    heap = Binheap()
+    assert len(heap._list) == 0
     assert heap.compare == heap.comp_min
 
 
 def test_binheap_init_max():
-    heap = binheap(minmax='max')
-    assert len(heap) == 0
+    heap = Binheap(minmax='max')
+    assert len(heap._list) == 0
     assert heap.compare == heap.comp_max
 
 
 def test_binheap_init_iter_min(init_list):
     raw, min_heap, max_heap = init_list
-    heap = binheap(raw, minmax="min")
-    assert heap == min_heap
+    heap = Binheap(raw, minmax="min")
+    assert heap._list == min_heap
 
 
 def test_binheap_init_iter_max(init_list):
     raw, min_heap, max_heap = init_list
-    heap = binheap(raw, minmax="max")
-    assert heap == max_heap
+    heap = Binheap(raw, minmax="max")
+    assert heap._list == max_heap
 
 
 def test_heapify_min(init_list):
     raw, min_heap, max_heap = init_list
-    heap = binheap(minmax="min")
-    heap.extend(raw)
+    heap = Binheap(minmax="min")
+    heap._list.extend(raw)
     heap._build()
-    assert heap == min_heap
+    assert heap._list == min_heap
 
 
 def test_heapify_max(init_list):
     raw, min_heap, max_heap = init_list
-    heap = binheap(minmax="max")
-    heap.extend(raw)
+    heap = Binheap(minmax="max")
+    heap._list.extend(raw)
     heap._build()
-    assert heap == max_heap
+    assert heap._list == max_heap
 
 
-def test_push_min(init_list):
+def test_push_min_large(init_list):
     raw, min_heap, max_heap = init_list
-    heap = binheap(minmax="min")
-    for n in raw:
-        heap.push(n)
-    assert heap == min_heap
+    heap = Binheap(min_heap, minmax="min")
+    heap.push(10)
+    assert heap._list == [-9, -3, 5, 7, 1, 10]
 
 
-def test_push_max(init_list):
+def test_push_min_small(init_list):
     raw, min_heap, max_heap = init_list
-    heap = binheap(minmax="max")
-    for n in raw:
-        heap.push(n)
-    assert heap == max_heap
+    heap = Binheap(min_heap, minmax="min")
+    heap.push(3)
+    assert heap._list == [-9, -3, 3, 7, 1, 5]
+
+
+def test_push_max_small(init_list):
+    raw, min_heap, max_heap = init_list
+    heap = Binheap(max_heap, minmax="max")
+    heap.push(3)
+    assert heap._list == [7, 1, 5, -3, -9, 3]
+
+
+def test_push_max_med(init_list):
+    raw, min_heap, max_heap = init_list
+    heap = Binheap(max_heap, minmax="max")
+    heap.push(6)
+    assert heap._list == [7, 1, 6, -3, -9, 5]
+
+
+def test_push_max_large(init_list):
+    raw, min_heap, max_heap = init_list
+    heap = Binheap(max_heap, minmax="max")
+    heap.push(10)
+    assert heap._list == [10, 1, 7, -3, -9, 5]
 
 
 def test_pop_min(init_list):
     raw, min_heap, max_heap = init_list
-    heap = binheap(raw, minmax="min")
+    heap = Binheap(raw, minmax="min")
     heap.pop()
-    assert heap == [-3, 1, 7, 5]
+    assert heap._list == [-3, 1, 5, 7]
 
 
 def test_pop_max(init_list):
     raw, min_heap, max_heap = init_list
-    heap = binheap(raw, minmax="max")
+    heap = Binheap(raw, minmax="max")
     heap.pop()
-    assert heap == [5, 1, -3, -9]
+    assert heap._list == [5, 1, -9, -3]
