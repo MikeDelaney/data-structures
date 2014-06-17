d9cbd1a281e1b2b0fde6e2594213c84b1ef3c500
Jonathan Siebert <jon.siebert@gmail.com>
2014-06-17 13:59:33 -0700

    added test_g_edges

--- test_graph.py
+++ test_graph.py
@@ -2,10 +2,20 @@
 
 import pytest
 
+
 def test_g_nodes():
-    graph = {node: edges for (node, edges) in \
+    graph = {node: edges for (node, edges) in
              [(hashable, []) for hashable in range(10)]
              }
     assert graph.nodes() == graph.keys()
 
 
+def test_g_edges():
+    graph = {node: edges for (node, edges) in
+             [(hashable, range(10)) for hashable in range(10)]
+             }
+    edges = []
+    for e in graph.values():
+        edges.extend(e)
+
+    assert graph.edges() == edges
