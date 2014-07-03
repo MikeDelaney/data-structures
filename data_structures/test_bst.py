5e2a71d82837480b8596ea1927a18b2f77ae0396
Jonathan Siebert <jon.siebert@gmail.com>
2014-07-03 13:17:51 -0700

    Added tests

--- data_structures/test_bst.py
+++ data_structures/test_bst.py
@@ -7,13 +7,13 @@ def test_init_bstree():
     assert tree.parent == None
     assert tree.left == None
     assert tree.right == float('inf')
-    assert tree.size == 1
+    assert tree.size() == 1
 
 
-def test_insert_insert_already_present():
+def test_insert_already_present():
     tree = BSTree(4)
     tree.insert(4)
-    assert tree.size == 1
+    assert tree.size() == 1
 
 def test_insert():
     tree = BSTree(4)
@@ -24,9 +24,30 @@ def test_insert():
     tree.insert(5)
     tree.insert(7)
     assert tree.key == 4
-    assert tree.left == 2
-    assert tree.right == 6
-    assert tree.left.left == 1
-    assert tree.left.right == 3
-    assert tree.right.left == 5
-    assert tree.right.right == 7
+    assert tree.left.key == 2
+    assert tree.right.key == 6
+    assert tree.left.left.key == 1
+    assert tree.left.right.key == 3
+    assert tree.right.left.key == 5
+    assert tree.right.right.key == 7
+
+
+def test_size():
+    tree = BSTree(4)
+    assert tree.size() == 1
+    tree.insert(2)
+    assert tree.size() == 2
+    tree.insert(6)
+    assert tree.size() == 3
+    tree.insert(1)
+    assert tree.size() == 4
+    tree.insert(3)
+    assert tree.size() == 5
+    tree.insert(5)
+    assert tree.size() == 6
+    tree.insert(7)
+    assert tree.size() == 7
+
+
+def test_depth():
+    
