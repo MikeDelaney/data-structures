f8268f40bc7ddce3647ff8254f6a54e20b35f6e7
MikeDelaney <mdelaney@utexas.edu>
2014-07-03 13:40:48 -0700

    Add stubs

---
+++ bst.py
@@ -0,0 +1,16 @@
+
+class BSTree(object):
+    def __init__(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def size(self):
+        pass
+
+    def depth(self):
+        pass
+
+    def balance(self):
+        pass
