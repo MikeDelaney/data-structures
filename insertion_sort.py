aa5f8b27cace6e41f0ffe2e913af421442131e25
MikeDelaney <mdelaney@utexas.edu>
2014-07-18 00:07:05 -0700

    Add main block to display time complexity of best/worst cases

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,8 +1,28 @@
 
-def insertion_sort(a_list):
-    for i in xrange(len(a_list)):
+def insertion_sort(sort_list):
+    for i in xrange(len(sort_list)):
         j = i
-        while j > 0 and a_list[j-1] > a_list[j]:
-            a_list[j], a_list[j-1] = a_list[j-1], a_list[j]
+        while j > 0 and sort_list[j-1] > sort_list[j]:
+            sort_list[j], sort_list[j-1] = sort_list[j-1], sort_list[j]
             j = j - 1
-    return a_list
+    return sort_list
+
+
+if __name__ == '__main__':
+    import timeit
+    worst_list = []
+    best_list = []
+    for i in [1, 5, 10, 50, 100, 500, 1000, 5000, 10000]:
+        worst = timeit.timeit('insertion_sort(range({}, -1, -1))'.format(i),
+                              'from __main__ import insertion_sort',
+                              number=1)
+        worst_list.append(worst)
+        best = timeit.timeit('insertion_sort(range({}))'.format(i),
+                             'from __main__ import insertion_sort',
+                             number=1)
+        best_list.append(best)
+    print '\nBest case scenario is sorting an already sorted list'
+    print 'Worst case scenario is sorting a list in reverse order'
+    print '\nBest Case\t\tWorst Case'
+    for i in xrange(len(best_list)):
+        print '{0}\t{1}'.format(best_list[i], worst_list[i])
