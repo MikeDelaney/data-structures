4f4f8e02912df3e3fc42740b96e258dd3c004856
MikeDelaney <mdelaney@utexas.edu>
2014-07-20 19:10:24 -0700

    Implement init method. Write stubs for methods.

---
+++ hashtable.py
@@ -0,0 +1,14 @@
+
+class HashTable(object):
+    def __init__(self, size):
+        self.size = size
+        self.buckets = [[] for i in xrange(size)]
+
+    def hash(self, key):
+        pass
+
+    def set(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
