d43636bf5e2f415fccd0834ca3cf47cd0771989f
MikeDelaney <mdelaney@utexas.edu>
2014-08-28 17:10:38 -0700

    Fix hashtable set method and tests to allow updating values

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -13,7 +13,12 @@ class HashTable(object):
     def set(self, key, value):
         if type(key) is not str:
             raise TypeError
-        self.buckets[self.hash(key)].append((key, value))
+        hash = self.hash(key)
+        for item in self.buckets[hash]:
+            if item[0] == key:
+                item[1] = value
+                return
+        self.buckets[hash].append([key, value])
 
     def get(self, key):
         for kv_pair in self.buckets[self.hash(key)]:
